65220068a6b73852d3274fa488affd33e7dfc79b
Aditya-Bhardwaj-99 <bhardwaj.aditya99@gmail.com>
2020-05-16 18:28:35 +0530

    done

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const AvcServer = require('./lib/server')
 const webss=require("ws").Server;
 const express=require("express");
 const app=express();
+const app2=express();
 var server=http.createServer(app);
-var wss= new webss({server:server,path:'/picam'});
+var server2=http.createServer(app2);
+var wss= new webss({server:server});
+var wss2=new webss({server:server2,path='/picam'});
 
 app.get('/',function(req,res){
     res.end("Server working");
@@ -23,7 +26,7 @@ avcServer.client_events.on('custom_event_from_client', e => {
     avcServer.broadcast('custom_event_from_server', { hello: 'from server' })
 })
 
-wss.on('connection',function(ws){
+wss2.on('connection',function(ws){
     ws.on('message',function(data){
         avcServer.setVideoStream(JSON.parse(data).data);
     })
