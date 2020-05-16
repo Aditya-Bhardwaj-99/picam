a8980a5c5354904a2fa3996b270163ad3c82d9eb
Aditya-Bhardwaj-99 <bhardwaj.aditya99@gmail.com>
2020-05-16 15:10:20 +0530

    new comm

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const AvcServer = require('../lib/server')
 const webss=require("ws").Server;
 const app=require("express")();
 var server=http.createServer(app);
-var wss= new webss({server:server, path:"/picam"});
+var wss= new webss({server:server});
 
 app.get('/',function(req,res){
     res.end("Server working");
